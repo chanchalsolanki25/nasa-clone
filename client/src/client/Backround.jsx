d29196d464362989000ee67c28ec0360ca085ffc
Chanchal Kumari <solankichanchal658@gmail.com>
2025-06-27 20:29:06 +0100

    background bug fixed

--- client/src/client/Backround.jsx
+++ client/src/client/Backround.jsx
@@ -9,7 +9,7 @@ function Backround() {
           className="fixed top-0 left-0 lg:w-full md:w-[768px] w-[640px] lg:h-full md:h-[600px] h-[350px] object-cover z-[-10]"
         >
           <source
-            src="src\assets\bg-video.mp4"
+            src="/bg-video.mp4"
             type="video/mp4"
             className="w-full h-full"
           />
